c228144aab5dd00c3c3231d9f4a3b3219dea1237
palashkumardas <info.palash27@gmail.com>
2023-07-23 18:53:49 +0600

    form prevent

--- src/components/modifier.vue
+++ src/components/modifier.vue
@@ -6,7 +6,15 @@ const space=ref(0);
 
 function spacePressed(){
     space.value++
-    message.value="Space press" + space.value + "times"
+    message.value="Space pressed " + space.value + " times"
+}
+function whatPressed(event){
+    message.value="You pressed " + event.key 
+
+}
+function clickButton(){
+    message.value="You Clicked The Button" 
+
 }
 </script>
 
@@ -16,7 +24,8 @@ function spacePressed(){
         <h2 class="mt-10">{{ message }}</h2>
         <form action="https://google.com/search">
             <div class="container mx-auto flex space-x-5 justify-center m-5"><input @keyup.space="spacePressed()" name="q" type="text" class="border border-gray-500 bg-white p-5" placeholder="Press Space"></div>
-            <div class="container mx-auto flex space-x-5 justify-center m-5"><input name="text" type="text" class="border border-gray-500 bg-white p-5" placeholder="Type Something"></div><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Button </button><button class="bg-blue-500 ml-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Ctrol + Right Click </button>
+            <div class="container mx-auto flex space-x-5 justify-center m-5"><input @keyup="whatPressed($event)" name="text" type="text" class="border border-gray-500 bg-white p-5" placeholder="Type Something"></div>
+            <button @click.prevent="clickButton()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Button </button><button class="bg-blue-500 ml-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Ctrol + Right Click </button>
         </form>
         <div class="bg-gray-200 w-[500px] h-80 mt-5 flex items-center justify-center"><button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"> Button </button></div>
     </section>
