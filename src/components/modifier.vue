f39974a78f31900c057ad2041bc42aa337554a4c
palashkumardas <info.palash27@gmail.com>
2023-07-23 19:06:13 +0600

    ctrlrightclick

--- src/components/modifier.vue
+++ src/components/modifier.vue
@@ -10,11 +10,12 @@ function spacePressed(){
 }
 function whatPressed(event){
     message.value="You pressed " + event.key 
-
 }
 function clickButton(){
     message.value="You Clicked The Button" 
-
+}
+function ctrlRightClick(){
+    message.value = "Ctrl+Right Click"
 }
 </script>
 
@@ -22,10 +23,11 @@ function clickButton(){
      <section class="container mx-auto flex items-center flex-col">
         <h1 class="text-center text-2xl py-10">Events &amp; Reactivity in Vue.js</h1>
         <h2 class="mt-10">{{ message }}</h2>
-        <form action="https://google.com/search">
+        <form @submit.prevent=""  action="https://google.com/search">
             <div class="container mx-auto flex space-x-5 justify-center m-5"><input @keyup.space="spacePressed()" name="q" type="text" class="border border-gray-500 bg-white p-5" placeholder="Press Space"></div>
             <div class="container mx-auto flex space-x-5 justify-center m-5"><input @keyup="whatPressed($event)" name="text" type="text" class="border border-gray-500 bg-white p-5" placeholder="Type Something"></div>
-            <button @click.prevent="clickButton()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Button </button><button class="bg-blue-500 ml-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Ctrol + Right Click </button>
+            <button @click.prevent="clickButton()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Button </button>
+            <button @click.ctrl="ctrlRightClick()" class="bg-blue-500 ml-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Ctrol + Right Click </button>
         </form>
         <div class="bg-gray-200 w-[500px] h-80 mt-5 flex items-center justify-center"><button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"> Button </button></div>
     </section>
