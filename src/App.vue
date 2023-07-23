edd7ecbb22ec6948c81f4c43d898d5fc132fd39f
palashkumardas <info.palash27@gmail.com>
2023-07-23 12:12:31 +0600

    accordion only open

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 // import location from './components/Location.vue'
-import objectReactivity from './components/objectReactivity.vue'
+// import objectReactivity from './components/objectReactivity.vue'
+import according from './components/according.vue'
 
 </script>
 
 <template>
-  <objectReactivity></objectReactivity>
+  <according></according>
 </template>
 
 <style scoped>
